a4c19ca6ca866b2a8efcc30c14d1fbbd40460d89
mechwire <jncmaguire@gmail.com>
2024-05-20 14:54:37 -0400

    first pass

---
+++ infra/variables.tf
@@ -0,0 +1,17 @@
+variable "repository_name" {
+  description = "the name of the repository"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "the name of the s3 bucket, with the TLD last, e.g.  cat.mammal.animal.com"
+  type        = string
+  default     = "wlpr.tech"
+}
+
+
+variable "s3_origin_id" {
+  description = "A random ID that apparently can be anything as long as its unique and we're consistent."
+  type        = string
+  default     = "s3-tech-wlpr-id"
+}
