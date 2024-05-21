35bea89173a3a3cecac0c7e5e662384500d58caa
mechwire <jncmaguire@gmail.com>
2024-05-20 21:21:15 -0400

    make user first

--- infra/output.tf
+++
@@ -1,9 +0,0 @@
-output "website_s3_bucket_arn" {
-  description = "The ARN for the bucket that holds the static assets"
-  value       = aws_s3_bucket.website_bucket.arn
-}
-
-output "cloudfront_distribution_id" {
-  description = "The ARN of the Cloudfront instance"
-  value       = aws_cloudfront_distribution.cdn_static_site.arn
-}
