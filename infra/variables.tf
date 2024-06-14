fb5a782f1340c79a40068a247befed1eb693bc55
mechwire <14144357+mechwire@users.noreply.github.com>
2024-06-14 15:21:05 -0400

    Force order for terraform and rename bucket to match future domain (#14)

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,12 +6,12 @@ variable "repository_name" {
 variable "bucket_name" {
   description = "the name of the s3 bucket, with the TLD last, e.g.  cat.mammal.animal.com"
   type        = string
-  default     = "wlpr.tech"
+  default     = "wlpr.dev"
 }
 
 
 variable "s3_origin_id" {
   description = "A random ID that apparently can be anything as long as its unique and we're consistent."
   type        = string
-  default     = "s3-tech-wlpr-id"
+  default     = "s3-wlpr-id"
 }
